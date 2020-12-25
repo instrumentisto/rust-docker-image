626feb49141b9f9ae6b7310c2ce448c9207ac96c
tyranron <tyranron@gmail.com>
2020-12-25 16:36:24 +0200

    Rework nightly images toolchain via GitHub Actions
    - populate nightly images to Quay.io and GitHub Container Registry

---
+++ nightly/check-changed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+if [ -z "$IMAGE_OS" ]; then
+  echo "error: No IMAGE_OS env var provided"
+  exit 1
+fi
+
+
+set -e
+
+
+upstream=$(skopeo inspect \
+                  --raw docker://docker.io/rustlang/rust:nightly-$IMAGE_OS \
+           | jq -c '.')
+echo "--> Upstream image manifests: $upstream"
+
+latest=$((skopeo inspect \
+                 --raw docker://docker.io/instrumentisto/rust:nightly-$IMAGE_OS\
+          || echo '"none"') \
+         | jq -c '.')
+echo "--> Latest image manifests: $latest"
+
+if [ "$upstream" != "$latest" ]; then
+  echo "--> CHANGED"
+fi
---
+++ nightly/copy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+runCmd() {
+  (set -x; $@)
+}
+
+
+if [ -z "$IMAGE_OS" ]; then
+  echo "error: No IMAGE_OS env var provided"
+  exit 1
+fi
+if [ -z "$IMAGE_TAGS" ]; then
+  echo "error: No $IMAGE_TAGS env var provided"
+  exit 1
+fi
+
+
+set -e
+
+runCmd \
+  podman pull rustlang/rust:nightly-$IMAGE_OS
+
+ver=$(podman run --rm rustlang/rust:nightly-$IMAGE_OS rustc -V \
+      | cut -d ' ' -f4 \
+      | tr -d "\n )")
+tags=$(printf "$IMAGE_TAGS" | sed "s/<ver>/$ver/g" | tr ',' "\n")
+repos=$(printf "$IMAGE_REPOS" | tr ',' "\n")
+
+for tag in $tags; do
+  for repo in $repos; do
+    runCmd \
+      skopeo copy --all "docker://docker.io/rustlang/rust:nightly-$IMAGE_OS" \
+                        "docker://$repo/instrumentisto/rust:$tag"
+  done
+done
